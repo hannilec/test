aac50dfb3cc5b1eb487faabfd15d52510cb69195
robak <robak@localhost.localdomain>
2020-07-16 03:36:10 +0200

    change to private

--- src/helpers/Customer.java
+++ src/helpers/Customer.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 public class Customer{
-    int id;
-    String name, email, city;
+    private int id;
+    private String name, email, city;
 
     Customer(int id, String name, String email, String city) {
         this.id = id;
@@ -16,7 +16,10 @@ public class Customer{
     }
 
     public Customer(List<WebElement> fields) {
-        this(Integer.parseInt(fields.get(0).getText()), fields.get(1).getText(), fields.get(2).getText(), fields.get(3).getText());
+        this(Integer.parseInt(fields.get(0).getText()),
+                fields.get(1).getText(),
+                fields.get(2).getText(),
+                fields.get(3).getText());
     }
 
     public int getId() {
